4656c21b95e51cf1f09bc6ba6ffe27d0ce592cce
mathlima1 <mathlima1@hotmail.com.br>
2021-03-18 23:31:17 -0300

    Migrando a aplicação para typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,9 +2,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import "../style/repository.scss"
 import { useState, useEffect } from "react";
 
+interface Repository {
+    name: string,
+    description: string,
+    html_url: string
+}
+
 export function RepositoryList() {
     //https://api.github.com/users/mathlima1/repos
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/users/mathlima1/repos')
